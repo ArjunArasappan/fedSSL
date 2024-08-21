85ba186de606b0eb1cd2f7fdb0c5563f0b1630cb
root <arasarjun@gmail.com>
2024-08-21 17:24:51 +0000

    gpu resources

--- fedSSL/server_app.py
+++ fedSSL/server_app.py
@@ -1,11 +1,16 @@
 """pytorchexample: A Flower / PyTorch app."""
 
 from typing import List, Tuple
-
+import flwr as fl
 from flwr.common import Context, Metrics, ndarrays_to_parameters
 from flwr.server import ServerApp, ServerAppComponents, ServerConfig
 from flwr.server.strategy import FedAvg
 
+import fedSSL.utils as utils
+from fedSSL.model import SimCLR
+from typing import List
+from collections import OrderedDict
+import torch
 import os
 
 
@@ -14,23 +19,24 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
     examples = [num_examples for num_examples, _ in metrics]
     return {"accuracy": sum(accuracies) / sum(examples)}
 
-class SaveModelStrategy(fl.server.strategy.FedAvg):
+class SaveModelStrategy(FedAvg):
     def aggregate_fit(self, server_round, results, failures):
         aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
 
         if aggregated_parameters is not None:
             
-            gb_simclr = SimCLR(DEVICE, useResnet18=useResnet18).to(DEVICE)
+            gb_simclr = SimCLR(utils.DEVICE, useResnet18=utils.useResnet18).to(utils.DEVICE)
 
             aggregated_ndarrays: List[np.ndarray] = fl.common.parameters_to_ndarrays(aggregated_parameters)
             params_dict = zip(gb_simclr.state_dict().keys(), aggregated_ndarrays)
             state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+            gb_simclr.load_state_dict(state_dict)
             
-            dir = './model_weights/'
+            dir = '.fedSSL/model_weights/'
             if not os.path.exists(dir):
                 os.makedirs(dir)
 
-            torch.save(state_dict, f"./model_weights/model_round_{server_round}.pth")
+            torch.save(gb_simclr.state_dict(), dir + f"model_round_{server_round}.pth")
 
         return aggregated_parameters, aggregated_metrics
     
