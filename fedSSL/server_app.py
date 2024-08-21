c59dff37f4bf59814fb2a7a1e09b9ae475de45ca
root <arasarjun@gmail.com>
2024-08-21 04:06:00 +0000

    updates

--- fedSSL/server_app.py
+++ fedSSL/server_app.py
@@ -6,6 +6,7 @@ from flwr.common import Context, Metrics, ndarrays_to_parameters
 from flwr.server import ServerApp, ServerAppComponents, ServerConfig
 from flwr.server.strategy import FedAvg
 
+import os
 
 
 def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
@@ -13,12 +14,31 @@ def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
     examples = [num_examples for num_examples, _ in metrics]
     return {"accuracy": sum(accuracies) / sum(examples)}
 
+class SaveModelStrategy(fl.server.strategy.FedAvg):
+    def aggregate_fit(self, server_round, results, failures):
+        aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
 
+        if aggregated_parameters is not None:
+            
+            gb_simclr = SimCLR(DEVICE, useResnet18=useResnet18).to(DEVICE)
+
+            aggregated_ndarrays: List[np.ndarray] = fl.common.parameters_to_ndarrays(aggregated_parameters)
+            params_dict = zip(gb_simclr.state_dict().keys(), aggregated_ndarrays)
+            state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+            
+            dir = './model_weights/'
+            if not os.path.exists(dir):
+                os.makedirs(dir)
+
+            torch.save(state_dict, f"./model_weights/model_round_{server_round}.pth")
+
+        return aggregated_parameters, aggregated_metrics
+    
 def server_fn(context: Context):
 
     num_rounds = context.run_config["num-server-rounds"]
 
-    strategy = FedAvg(
+    strategy = SaveModelStrategy(
         fraction_fit=1.0,
         fraction_evaluate=context.run_config["fraction-evaluate"],
         min_available_clients=2,
